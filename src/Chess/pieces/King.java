d3e52a353091b960e3ec9f67fccac129f7bc9191
rafa <ralfrr55@gmail.com>
2022-07-08 08:49:22 -0300

    movimentos king

--- src/Chess/pieces/King.java
+++ src/Chess/pieces/King.java
@@ -3,23 +3,79 @@ package Chess.pieces;
 import Chess.ChessPiece;
 import Chess.Color;
 import boardGame.Board;
+import boardGame.Position;
 
 public class King extends ChessPiece {
 
-
-	public King(Board board,  Color color) {
+	public King(Board board, Color color) {
 		super(board, color);
-	} 
-	
+	}
+
 	@Override
 	public String toString() {
 		return "K";
 	}
 
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// above
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExist(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExist(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// lefth
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExist(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExist(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// nw
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExist(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// ne
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExist(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// sw
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExist(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// se
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExist(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
 		return mat;
 	}
-	
+
 }
--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -32,6 +32,21 @@ public class UI {
 		System.out.print("  a b c d e f g h ");
 	}
 
+
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves ) {
+
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j]);
+			}
+			System.out.println();
+		}
+
+		System.out.print("  a b c d e f g h ");
+	}
+
+	
 	private static void printPiece(ChessPiece piece) {
 
 		if (piece == null) {
