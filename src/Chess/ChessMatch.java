d88f9d1f274d90ed7c6ab5cd004e02276ac8b424
rafa <ralfrr55@gmail.com>
2022-07-09 16:58:00 -0300

    capturando pecas

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package Chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Chess.pieces.King;
 import Chess.pieces.Rook;
 import boardGame.Board;
@@ -12,6 +15,9 @@ public class ChessMatch {
 
 	private int turn;
 	private Color currentPlayer;
+	
+	private List<ChessPiece> piecesOnTheBoard = new ArrayList<>();
+	private List<ChessPiece> capturePieces = new ArrayList<>();
 
 	public ChessMatch() {
 
@@ -63,6 +69,12 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
+		if(capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece); 
+			capturePieces.addAll(capturePieces);
+		}
+		
 		return capturedPiece;
 	}
 
@@ -96,6 +108,7 @@ public class ChessMatch {
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 
 	public void initialSetup() {
