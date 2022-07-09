15b06d81529739618a9fe6cf3d671729e45b7a99
rafa <ralfrr55@gmail.com>
2022-07-09 16:16:45 -0300

    trocando de jogador

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -10,10 +10,23 @@ public class ChessMatch {
 
 	private Board board;
 
+	private int turn;
+	private Color currentPlayer;
+
 	public ChessMatch() {
 
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
+	}
+	
+	public int getTurn() {
+		return turn;	
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -26,66 +39,78 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-	
-	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
 		Position position = sourcePosition.toPosition();
 		validationSourcePosition(position);
 		return board.piece(position).possibleMoves();
 	}
-	
+
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
-		
+
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validationSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMovie(source, target);
-		return (ChessPiece)capturedPiece; 
-		
+		nextTurn();
+		return (ChessPiece) capturedPiece;
+
 	}
-	
-	private Piece  makeMovie(Position source, Position target) {
-		
+
+	private Piece makeMovie(Position source, Position target) {
+
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		return capturedPiece;
 	}
-	
-	
+
 	private void validationSourcePosition(Position position) {
-		if(!board.thereIsAPiece(position)) {
+		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is  no piece on source position");
 		}
-		if(!board.piece(position).isThereAnyPossibleMovie()) {
+		
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+			
+			throw new ChessException("The chosen piece is not yours");
+			
+		}
+		
+		if (!board.piece(position).isThereAnyPossibleMovie()) {
 			throw new ChessException("There is possible Movies for chose piece");
 		}
 	}
-	
+
 	public void validateTargetPosition(Position source, Position target) {
-		if(!board.piece(source).possibleMove(target)) {
+		if (!board.piece(source).possibleMove(target)) {
 			throw new ChessException("The chosen piece cant move totarget position");
 		}
 	}
 	
-	
-	private void placeNewPiece(char column, int row, ChessPiece piece) {
-		
-		board.placePiece(piece, new ChessPosition(column, row).toPosition());		
+	public void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 
-	public void initialSetup() {     placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-    placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-    placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-    placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-    placeNewPiece('d', 1, new King(board, Color.WHITE));
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
 
-    placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-    placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-    placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-    placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-    placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-    placeNewPiece('d', 8, new King(board, Color.BLACK));
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+
+	public void initialSetup() {
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 
 }
--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -3,6 +3,7 @@ package aplication;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.ChessPosition;
 
@@ -17,6 +18,13 @@ public class UI {
 		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reding Chesspoint valid values are from a1 a h8");
 		}
+	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.println("Wating player: " + chessMatch.getCurrentPlayer());
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
